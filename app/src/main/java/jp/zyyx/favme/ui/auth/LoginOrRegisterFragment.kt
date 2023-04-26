e64deffcf68595c69e9e0f9a2360907a7bcf003d
lebatiendat <lebatiendat@thang-mac-m1.local>
2023-04-26 16:44:25 +0700

    update

--- app/src/main/java/jp/zyyx/favme/ui/auth/LoginOrRegisterFragment.kt
+++ app/src/main/java/jp/zyyx/favme/ui/auth/LoginOrRegisterFragment.kt
@@ -6,18 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import jp.zyyx.favme.R
+import jp.zyyx.favme.databinding.FragmentLoginBinding
+import jp.zyyx.favme.repository.AuthRepository
+import jp.zyyx.favme.ui.base.BaseFragment
 
-class LoginOrRegisterFragment : Fragment() {
-
-
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_or_register, container, false)
+class LoginOrRegisterFragment : BaseFragment<AuthViewModel, FragmentLoginBinding, AuthRepository>() {
+    override fun getViewModel(): Class<AuthViewModel> {
+        TODO("Not yet implemented")
     }
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentLoginBinding {
+        TODO("Not yet implemented")
+    }
+
+    override fun getFragmentRepository(): AuthRepository {
+        TODO("Not yet implemented")
+    }
+
 
 }
