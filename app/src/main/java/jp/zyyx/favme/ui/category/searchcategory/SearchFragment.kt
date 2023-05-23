7de093f76f5d0eea94caa615113a9c262a786629
lebatiendat <lebatiendat@les-MacBook-Pro.local>
2023-05-22 21:22:29 -0700

    update

---
+++ app/src/main/java/jp/zyyx/favme/ui/category/searchcategory/SearchFragment.kt
@@ -0,0 +1,45 @@
+package jp.zyyx.favme.ui.category.searchcategory
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import jp.zyyx.favme.base.BaseFragment
+import jp.zyyx.favme.databinding.FragmentSearchBinding
+import jp.zyyx.favme.extension.popBackStack
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(
+    FragmentSearchBinding::inflate
+) {
+
+
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentSearchBinding.inflate(layoutInflater, container, false)
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            requireActivity().popBackStack()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        handleObservable()
+    }
+
+    private fun initView() {
+
+
+    }
+
+    private fun handleObservable() {
+
+    }
+
+}
